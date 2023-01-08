4288ea8f78e22e46a59792ff64a1556ddce1c114
Mitsuha <caizhe666@qq.com>
2023-01-08 19:17:23 +0800

    Site updated: 2023-01-08 19:17:23

--- js/third-party/fancybox.js
+++ js/third-party/fancybox.js
@@ -1,9 +1,1 @@
-document.addEventListener("page:loaded",(()=>{
-/**
-   * Wrap images with fancybox.
-   */
-document.querySelectorAll(".post-body :not(a) > img, .post-body > img").forEach((t=>{const a=$(t),e=a.attr("data-src")||a.attr("src"),r=a.wrap(`<a class="fancybox fancybox.image" href="${e}" itemscope itemtype="http://schema.org/ImageObject" itemprop="url"></a>`).parent("a");a.is(".post-gallery img")?r.attr("data-fancybox","gallery").attr("rel","gallery"):a.is(".group-picture img")?r.attr("data-fancybox","group").attr("rel","group"):r.attr("data-fancybox","default").attr("rel","default");const o=a.attr("title")||a.attr("alt");o&&(
-// Do not append image-caption if pandoc has already created a figcaption
-r.next("figcaption").length||r.append(`<p class="image-caption">${o}</p>`),
-// Make sure img title tag will show correctly in fancybox
-r.attr("title",o).attr("data-caption",o))})),$.fancybox.defaults.hash=!1,$(".fancybox").fancybox({loop:!0,helpers:{overlay:{locked:!1}}})}));
+document.addEventListener("page:loaded",(()=>{document.querySelectorAll(".post-body :not(a) > img, .post-body > img").forEach((a=>{var t=(a=$(a)).attr("data-src")||a.attr("src");t=a.wrap(`<a class="fancybox fancybox.image" href="${t}" itemscope itemtype="http://schema.org/ImageObject" itemprop="url"></a>`).parent("a");(a=(a.is(".post-gallery img")?t.attr("data-fancybox","gallery").attr("rel","gallery"):a.is(".group-picture img")?t.attr("data-fancybox","group").attr("rel","group"):t.attr("data-fancybox","default").attr("rel","default"),a.attr("title")||a.attr("alt")))&&(t.next("figcaption").length||t.append(`<p class="image-caption">${a}</p>`),t.attr("title",a).attr("data-caption",a))})),$.fancybox.defaults.hash=!1,$(".fancybox").fancybox({loop:!0,helpers:{overlay:{locked:!1}}})}));
